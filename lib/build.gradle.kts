2ed8ae6346bdc6985a2369dee9c851f1d9533683
Wenxi Zeng <wenxi.zeng@segment.com>
2022-02-23 11:32:59 -0800

    add dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 
 repositories {
     // Use JCenter for resolving dependencies.
+    google()
     mavenCentral()
 }
 
@@ -24,5 +25,29 @@ dependencies {
 
     implementation("com.segment.analytics.kotlin:core:1.4.3")
 
+}
+
+// Test Dependencies
+dependencies {
+    testImplementation ("junit:junit:4.13.2")
+    testImplementation ("androidx.test.ext:junit:1.1.3")
+    testImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+
+    testImplementation ("io.mockk:mockk:1.10.6")
+    testImplementation(platform("org.junit:junit-bom:5.7.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Add Roboelectric dependencies.
+    testImplementation ("org.robolectric:robolectric:4.7.3")
+    testImplementation ("androidx.test:core:1.4.0")
+
+    // Add JUnit4 legacy dependencies.
+    testImplementation ("junit:junit:4.13.2")
+    testRuntimeOnly ("org.junit.vintage:junit-vintage-engine:5.7.2")
+
+    // For JSON Object testing
+    testImplementation ("org.json:json:20200518")
+    testImplementation ("org.skyscreamer:jsonassert:1.5.0")
 }
 
